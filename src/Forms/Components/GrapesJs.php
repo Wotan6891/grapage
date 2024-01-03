9fc355bbcbb1b7a2c7b67828eab57e4a9aea70ef
Wotan6891 <Wotan6891@users.noreply.github.com>
2024-01-03 16:15:17 +0000

    Fix styling

--- src/Forms/Components/GrapesJs.php
+++ src/Forms/Components/GrapesJs.php
@@ -6,15 +6,16 @@ use Closure;
 use Ekremogul\FilamentGrapesjs\Forms\Components\Concerns\InteractsWithTools;
 use Filament\Forms\Components\Field;
 
-
 class GrapesJs extends Field
 {
     use InteractsWithTools;
+
     protected string $view = 'filament-grapesjs::forms.components.fields.grapesjs';
 
     protected array | Closure $tools = [
 
     ];
+
     protected string $htmlData;
 
     protected int | Closure | null $minHeight = 768;
